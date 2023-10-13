841df1e8fb7337c80658a9a6bab028b9ff5406e2
Nathan Wilson <nathan@collectivesource.com>
2023-10-13 08:06:24 -0400

    Have bids deduct from user balance and fail if balance would go below 0

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -4,7 +4,12 @@ class BidsController < ApplicationController
   def create
     @item = Item.find(params[:item_id])
     @user = User.find_by(code: params[:bid][:code])
-    @bid = @item.bids.create(user: @user, amount: params[:bid][:amount])
+    amount = params[:bid][:amount]
+    if @user.deduct(amount)
+      @bid = @item.bids.create(user: @user, amount:)
+    else
+      flash.alert = "#{@user.name} does not have enough Sanctuary Boston Bucks"
+    end
     redirect_to item_path(@item)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   has_many :bids
   validates :code, presence: true
   validates :name, presence: true
+
+  def deduct(amount)
+    safe_amount = amount.to_i
+    return false if balance < safe_amount
+    return false if safe_amount <= 0
+
+    self.balance -= amount.to_i
+    save
+  end
 end
