61aa4a8c9fed6f479b03056b0065582123d86849
Nathan Wilson <nathan@collectivesource.com>
2023-10-13 08:36:22 -0400

    Full coverage

--- test/controllers/bids_controller_test.rb
+++ test/controllers/bids_controller_test.rb
@@ -8,7 +8,16 @@ class BidsControllerTest < ActionDispatch::IntegrationTest
     post(item_bids_path(item_id: items(:one).id),
          params: { bid: { code: users(:one).code, amount: '100' } })
     assert_response :redirect
-    follow_redirect!
     assert_equal(before + 1, Bid.count)
+  end
+
+  test 'create fail over budget' do
+    before = Bid.count
+    user = users(:one)
+    post(item_bids_path(item_id: items(:one).id),
+         params: { bid: { code: user.code, amount: user.balance + 1 } })
+    assert(1, flash.count)
+    assert_response :redirect
+    assert_equal(before, Bid.count)
   end
 end
