5146b00ecbfcadf8a79ee2e3a7ece3f624695b4f
Nathan Wilson <nathan@collectivesource.com>
2023-10-12 21:05:29 -0400

    Add Bid object

---
+++ db/migrate/20231013010450_create_bids.rb
@@ -0,0 +1,11 @@
+class CreateBids < ActiveRecord::Migration[7.1]
+  def change
+    create_table :bids do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :item, null: false, foreign_key: true
+      t.integer :amount
+
+      t.timestamps
+    end
+  end
+end
