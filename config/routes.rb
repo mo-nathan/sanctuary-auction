6f50bc2093c5099040e0a6f487df188014e7fa0b
Nathan Wilson <nathan@collectivesource.com>
2023-10-12 20:33:01 -0400

    CRUD for items

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "items#index"
 
-  get "/items", to: "items#index"
+  resources :items
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
