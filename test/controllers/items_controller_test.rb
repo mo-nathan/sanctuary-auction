e4b2b0e87976c79b99ff527cf459c389744526fa
Nathan Wilson <nathan@collectivesource.com>
2023-10-13 05:44:26 -0400

    Some ItemsController tests

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -8,4 +8,37 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
     get(item_path(id: item.id))
     assert_equal(200, @response.status)
   end
+
+  test 'index' do
+    get(items_path)
+    assert_equal(200, @response.status)
+  end
+
+  test 'new' do
+    get(new_item_path)
+    assert_equal(200, @response.status)
+  end
+
+  test 'create' do
+    before = Item.count
+    post(items_path,
+         params: { item: { description: "can create" } })
+    assert_response :redirect
+    follow_redirect!
+    assert_equal(before + 1, Item.count)
+  end
+
+  test 'create fail' do
+    before = Item.count
+    post(items_path,
+         params: { item: { description: "" } })
+    assert_response :unprocessable_entity
+    assert_equal(before, Item.count)
+  end
+
+  test 'edit' do
+    item = items(:one)
+    get(edit_item_path(id: item.id))
+    assert_equal(200, @response.status)
+  end
 end
