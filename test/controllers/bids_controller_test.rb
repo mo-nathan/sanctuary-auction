44121e958d3a07ad4386edc6e0246063df13ae4d
Nathan Wilson <nathan@collectivesource.com>
2023-10-13 06:27:29 -0400

    Rubocop

--- test/controllers/bids_controller_test.rb
+++ test/controllers/bids_controller_test.rb
@@ -6,7 +6,7 @@ class BidsControllerTest < ActionDispatch::IntegrationTest
   test 'create' do
     before = Bid.count
     post(item_bids_path(item_id: items(:one).id),
-         params: { bid: { code: users(:one).code, amount: "100" } })
+         params: { bid: { code: users(:one).code, amount: '100' } })
     assert_response :redirect
     follow_redirect!
     assert_equal(before + 1, Bid.count)
