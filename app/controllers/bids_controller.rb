e884526d4622c9586c5b603aefd938904d45727a
Nathan Wilson <nathan@collectivesource.com>
2023-10-13 20:12:50 -0400

    Replace bids

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -4,15 +4,24 @@ class BidsController < ApplicationController
   def create
     @item = Item.find(params[:item_id])
     @user = User.find_by(code: params[:bid][:code])
+    delete_old_bid(@item, @user)
     deduct(params[:bid][:amount].to_i)
     redirect_to item_path(@item)
   end
 
   private
 
+  def delete_old_bid(item, user)
+    bid = Bid.find_by(item:, user:)
+    return unless bid
+
+    user.balance += bid.amount
+    bid.delete
+  end
+
   def deduct(amount)
     if @user.deduct(amount)
-      @bid = @item.bids.create(user: @user, amount:)
+      @bid = @item.bids.create(user: @user, amount:) if amount.positive?
     else
       flash.alert = "#{@user.name} only has #{@user.balance} Sanctuary Boston Bucks"
     end
