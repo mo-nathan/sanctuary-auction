dc71b023541a27cba0a900c6610abfc504833a5a
Nathan Wilson <nathan@collectivesource.com>
2023-10-14 18:54:05 -0400

    Ruboop

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -4,16 +4,20 @@ class BidsController < ApplicationController
   def create
     @item = Item.find(params[:item_id])
     @user = User.find_by(code: params[:bid][:code])
-    if @user
-      delete_old_bid(@item, @user)
-      deduct(params[:bid][:amount].to_i)
-    else
-      flash.alert = "Unable to find a user with the code #{params[:bid][:code]}"
-    end
+    update_user(@item, @user)
     redirect_to item_path(@item)
   end
 
   private
+
+  def update_user(item, user)
+    if user
+      delete_old_bid(item, user)
+      deduct(item, user, params[:bid][:amount].to_i)
+    else
+      flash.alert = "Unable to find a user with the code #{params[:bid][:code]}"
+    end
+  end
 
   def delete_old_bid(item, user)
     bid = Bid.find_by(item:, user:)
@@ -23,11 +27,11 @@ class BidsController < ApplicationController
     bid.delete
   end
 
-  def deduct(amount)
-    if @user.deduct(amount)
-      @bid = @item.bids.create(user: @user, amount:) if amount.positive?
+  def deduct(item, user, amount)
+    if user.deduct(amount)
+      item.bids.create(user:, amount:) if amount.positive?
     else
-      flash.alert = "#{@user.name} only has #{@user.balance} Sanctuary Boston Bucks"
+      flash.alert = "#{user.name} only has #{user.balance} Sanctuary Boston Bucks"
     end
   end
 end
