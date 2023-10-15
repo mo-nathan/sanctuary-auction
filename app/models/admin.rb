7c2485863eddafbe17c7c58e9f36c1c9296e79c1
Nathan Wilson <nathan@collectivesource.com>
2023-10-15 07:12:30 -0400

    Only allow one admin account

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 class Admin < ApplicationRecord
+  validate :admin_check
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def admin_check
+    if Admin.all.size.positive?
+      errors.add(:base, "There Can Be Only One (Admin)")
+    end
+  end
 end
