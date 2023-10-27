c5fb63dcded3ac09ccd61031aa8a63c946f77ef3
Nathan Wilson <nathan@collectivesource.com>
2023-10-27 07:20:01 -0400

    Add Welcome page, switch from Users to Participants, add some documentation, add deploy script

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,16 @@
 
 Rails.application.routes.draw do
   devise_for :admins
-  root 'items#index'
+  root 'welcome#index'
+
+  get 'welcome/index'
 
   resources :items do
     resources :bids
   end
   resources :users
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
