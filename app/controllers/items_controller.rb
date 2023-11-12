d1e8d3e4acf5672f429e3d5051fc52e130ffba8d
Nathan Wilson <nathan@collectivesource.com>
2023-11-12 11:27:15 -0500

    Add host to items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,6 +50,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:description, :cost, :number, :image_url, :auction, :title)
+    params.require(:item).permit(:host, :description, :cost, :number, :image_url, :auction, :title)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_27_005418) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_12_161848) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_27_005418) do
     t.string "image_url"
     t.boolean "auction", default: false
     t.string "title", default: "An Item", null: false
+    t.string "host"
   end
 
   create_table "users", force: :cascade do |t|
